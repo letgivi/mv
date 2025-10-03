05869fcae829a8b25844911468661b205da7df68
letgivi <94454048+letgivi@users.noreply.github.com>
2025-10-03 18:50:27 +0700

    Update sn.js

--- sn.js
+++ sn.js
@@ -1,23 +1,26 @@
-  var jssor_slider1;
+var jssor_slider1;
+
   function jssor_slider1_starter(containerId) {
     var options = {
-      $AutoPlay: 1,
-      $SlideDuration: 500,
-      $DragOrientation: 3
+      $AutoPlay: 1,            // Tự động chạy
+      $SlideDuration: 500,     // Thời gian chuyển slide
+      $DragOrientation: 3      // Cho phép kéo ngang/dọc
     };
     jssor_slider1 = new $JssorSlider$(containerId, options);
   }
 
+  // Khởi tạo slider
   jssor_slider1_starter("slider1_container");
 
+  // Responsive fix
   function ScaleSlider() {
     var container = document.getElementById("slider1_container");
     if (window.innerWidth <= 768) {
-      container.style.height = "320px"; // mobile: cao 320px
-      jssor_slider1.$ScaleWidth(container.clientWidth); 
+      container.style.height = "320px";
+      jssor_slider1.$ScaleWidth(container.clientWidth);
     } else {
-      container.style.height = "320px"; // PC vẫn cao 320px
-      jssor_slider1.$ScaleWidth(820); 
+      container.style.height = "320px";
+      jssor_slider1.$ScaleWidth(820);
     }
   }
 
